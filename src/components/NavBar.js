210aacc138608606e052232178d505da2efaa1e9
Emily Bender <bender.emily1@gmail.com>
2020-02-08 16:55:27 -0500

    NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Pokédex
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
